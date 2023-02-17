528f2edb088333708acd4784acc6a3f9cad1a4b7
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2023-02-17 17:29:10 -0500

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 // Express Settings
 app.use(cors());
@@ -11,12 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'public', 'build')));
+}
+
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/places", require("./controllers/places"));
 app.use("/users", require("./controllers/users"));
+
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
